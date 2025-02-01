b7c6e0ab438390eb2302f5e0b602d68382362692
SolidSnakeEater <noreply@google.com>
2025-02-01 20:52:33 +0530

    update test for hasher public interface

--- src/hasher.rs
+++ src/hasher.rs
@@ -20,7 +20,7 @@ pub trait HashProvider {
         salt: &str,
         rounds: &u32,
         kind: Self::Kind,
-    ) -> Result<String, DefaultError>;
+    ) -> Result<HasherResult, DefaultError>;
 }
 
 pub struct PBKDF2HashProvide {}
@@ -34,7 +34,7 @@ impl HashProvider for PBKDF2HashProvide {
         salt: &str,
         rounds: &u32,
         kind: Self::Kind,
-    ) -> Result<String, DefaultError> {
+    ) -> Result<HasherResult, DefaultError> {
         match kind {
             HasherKind::PBKDF2 => {
                 tracing::info!("PBKDF2");
@@ -44,14 +44,36 @@ impl HashProvider for PBKDF2HashProvide {
 
                 let hasher = &mut pbkdf2::HashProvider::<pbkdf2::PrfHasher>::new(&mut buf_boxed);
                 let hash = hasher.pbkdf2_gen(password, salt, &rounds).unwrap();
+                let hash_hex = hex::encode(hash);
 
-                Ok(hex::encode(hash))
+                Ok(HasherResult::new(
+                    hash_hex,
+                    password.to_string(),
+                    salt.to_string(),
+                ))
             }
             HasherKind::Argon2 => {
                 tracing::info!("Argon2");
 
-                Ok("".to_string())
+                Ok(HasherResult::default())
             }
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct HasherResult {
+    pub hash: String,
+    pub password: String,
+    pub salt: String,
+}
+
+impl HasherResult {
+    pub fn new(hash: String, password: String, salt: String) -> Self {
+        Self {
+            hash,
+            password,
+            salt,
         }
     }
 }
@@ -62,20 +84,31 @@ mod tests {
     use super::*;
     use tracing_test::traced_test;
 
+    use prettytable::row;
+    use prettytable::Table;
+
     const HASH_ROUNDS: u32 = 2;
 
     #[traced_test]
     #[test]
-    fn get_hasher_with_impl_trait() {
+    fn pbkdf2_hasher_with_impl_trait() {
         let pbkdf2 = PBKDF2HashProvide {};
+        let mut table = Table::new();
 
-        fn get_hasher(hasher: impl HashProvider<Kind = HasherKind>) -> String {
+        fn get_hasher(hasher: impl HashProvider<Kind = HasherKind>) -> HasherResult {
             hasher
                 .hash("password", "salt", &HASH_ROUNDS, HasherKind::PBKDF2)
                 .unwrap()
         }
 
         let hash = get_hasher(pbkdf2);
-        assert_ne!(hash, "".to_string());
+        assert_ne!(hash.hash, "".to_string());
+
+        let pbkdf_key_details = format!("PBKDF2 / SHA-512 with {} rounds", HASH_ROUNDS);
+        table.add_row(row![pbkdf_key_details.as_str(), hash.hash]);
+        table.add_row(row!["Password", hash.password]);
+        table.add_row(row!["Salt", hash.salt]);
+
+        // table.printstd();
     }
 }
