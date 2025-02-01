c7c217ed173332fb1787522304e0f3fb4a8b573f
SolidSnakeEater <noreply@google.com>
2025-02-01 16:45:51 +0530

    hide internal impl

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ use aes_gcm_siv::AesGcmSiv;
 use prelude::AesEncrypt;
 use tracing::trace;
 
-pub mod aes;
+pub(crate) mod aes;
 pub mod encrypter;
 pub mod error;
 pub mod hasher;
 pub mod prelude {
-    pub use crate::aes::AesEncrypt;
+    pub(crate) use crate::aes::AesEncrypt;
 }
