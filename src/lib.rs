2cfe1b60bc6ceb7e143d587828239e3d6a541c5b
SolidSnakeEater <noreply@google.com>
2025-02-02 21:28:17 +0530

    more cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 use crate::error::DefaultError;
 
+// NOTE: this needs to be migrated, that's a lot of unnecessary code due to a speedrun
 pub(crate) mod aes;
-
-// NOTE: These are now used as references of a crude speed run, and I'm extracting these into the
-// public interface, with simplifcation as the goal.
-pub(crate) mod encrypter_internal;
-pub(crate) mod hasher_internal;
 
 pub mod decrypter;
 pub mod encrypter;
