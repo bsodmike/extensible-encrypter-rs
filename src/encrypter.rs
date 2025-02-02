a8c9ab3047fd77bfea8a69d5ec1c91681de2ca3a
SolidSnakeEater <noreply@google.com>
2025-02-03 00:07:01 +0530

    fix: perform encryption and decryption, finally, it's working again

--- src/encrypter.rs
+++ src/encrypter.rs
@@ -1,15 +1,11 @@
 use crate::error::DefaultError;
 use crate::hasher::{HashProvider, HasherKind};
+use aes_gcm_siv::aead::rand_core::RngCore;
 use aes_gcm_siv::{
     aead::{Aead, KeyInit, OsRng},
     Aes256GcmSiv, Nonce,
 };
-use hmac::Hmac;
-use pbkdf2::{
-    password_hash::{PasswordHasher, SaltString},
-    Pbkdf2,
-};
-use sha2::Sha512;
+use pbkdf2::password_hash::SaltString;
 
 pub mod aes256_gcm_siv;
 
@@ -49,39 +45,50 @@ impl EncryptProvider for Aes256GcmSivEncryptProvide {
             EncrypterKind::Aes256GcmSiv => {
                 tracing::info!("Aes256GcmSiv");
 
-                let plaintext = "Hello, world!";
+                let plaintext = "secret nuke codes go inside the football";
 
                 // A salt for PBKDF2 (should be unique per encryption)
                 let salt = SaltString::generate(&mut OsRng);
+                let salt_hex = hex::encode(salt.as_ref());
+                tracing::debug!("Salt: {}", &salt);
+
+                // FIXME: Similar to the decrypter, we need to pass the salt
+                // let hasher = crate::hasher::Hasher::hash(
+                //     password,
+                //     &HASH_ROUNDS,
+                //     hash_provider,
+                //     HasherKind::PBKDF2,
+                // );
+                // assert_ne!(hasher.hash, "".to_string());
 
                 // Derive a 32-byte key using PBKDF2 with SHA-512 and 20 rounds
-                let hasher = crate::hasher::Hasher::hash(
-                    password,
+                let hasher = super::hasher::pbkdf2::Hasher::hash(
+                    "password",
                     &HASH_ROUNDS,
-                    hash_provider,
-                    HasherKind::PBKDF2,
-                );
-                assert_ne!(hasher.hash, "".to_string());
+                    super::hasher::pbkdf2::Algorithm::Pbkdf2Sha256,
+                    Some(salt),
+                )
+                .unwrap();
 
                 // Convert the key to a fixed-size array
-                let key = hex::decode(hasher.hash.as_str()).unwrap();
+                let key = hex::decode(hasher.hash().as_str()).unwrap();
                 let key_array: [u8; 32] = key.try_into().unwrap();
 
                 // Initialize the AES-GCM-SIV cipher
                 let cipher = Aes256GcmSiv::new_from_slice(&key_array).unwrap();
 
                 // Generate a random nonce (96 bits)
-                let nonce = aes256_gcm_siv::generate_nonce(hasher.hash);
+                let mut nonce = [0u8; 12]; // 96 bits = 12 bytes
+                OsRng.fill_bytes(&mut nonce);
+                let nonce = Nonce::from_slice(&nonce); // Convert to Nonce type
 
                 // Encrypt the message
                 let ciphertext = cipher.encrypt(&nonce, plaintext.as_bytes()).unwrap(); // Output the results
-                tracing::debug!("Salt: {}", salt);
                 tracing::debug!("Nonce: {:?}", nonce);
                 tracing::debug!("Ciphertext: {:?}", ciphertext);
 
                 let ciphertext_hex = hex::encode(ciphertext);
                 let nonce_hex = hex::encode(nonce);
-                let salt_hex = hex::encode(salt.as_ref());
 
                 Ok(EncryptionResult {
                     ciphertext: ciphertext_hex,
@@ -133,24 +140,7 @@ mod tests {
 
     #[traced_test]
     #[test]
-    fn aes256_gcm_siv_with_impl_trait() {
-        let hash_provider = PBKDF2HashProvide {};
-        let provider = Aes256GcmSivEncryptProvide {};
-
-        let result = Encrypter::encrypt(
-            "password",
-            hash_provider,
-            provider,
-            EncrypterKind::Aes256GcmSiv,
-            HasherKind::PBKDF2,
-        );
-        tracing::info!("Result: {:?}", result);
-    }
-
-    #[ignore]
-    #[traced_test]
-    #[test]
-    fn aes256_gcm_siv_with_impl_trait_integration() {
+    fn aes256_gcm_siv_e2e() {
         let hash_provider = PBKDF2HashProvide {};
         let provider = Aes256GcmSivEncryptProvide {};
 
