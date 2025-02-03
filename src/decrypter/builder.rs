e3f4adc031c224cd5d54ffcb71bafcbb5f2c080c
SolidSnakeEater <noreply@google.com>
2025-02-03 14:34:35 +0530

    cleanup warnings

--- src/decrypter/builder.rs
+++ src/decrypter/builder.rs
@@ -34,12 +34,14 @@ impl DecrypterPayload for &mut Decrypter {
     }
 }
 
+#[allow(dead_code)]
 pub struct DecrypterBuilder {
     salt: String,
     nonce: String,
     ciphertext: String,
 }
 
+#[allow(dead_code)]
 impl DecrypterBuilder {
     pub fn new() -> Self {
         Self {
